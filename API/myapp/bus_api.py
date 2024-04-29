9cbdba472782d4eec435a798b430b6ad32bc4e85
Michael Terrefortes <michael.terrefortes@upr.edu>
2024-04-29 13:02:32 -0400

    api returns the eta

--- API/myapp/bus_api.py
+++ API/myapp/bus_api.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify, render_template, current_app, url_for, redirect, Flask, make_response
-from .database import db, Bus
+from .database import db, Bus, ETA
 import json
 import os
 import uuid
@@ -29,6 +29,47 @@ def receive_gps_data():
     db.session.commit()
 
     return jsonify({"message": "GPS data received and saved successfully"}), 201
+
+
+@bus.route('/get_eta', methods=['GET'])
+def get_eta():
+    data = request.json
+    stop = data['stop']
+    end_stop = data['end_stop']
+    time_slot = data['time_slot']
+    
+    if stop is None or time_slot is None:
+        return jsonify({"message": "Select stop and a time slot"}), 400
+
+    row =  ETA.query.filter_by(time_slot=time_slot).all()
+
+    eta_no_bus = 0
+
+    route = [4, 5, 6, 13, 24, 23, 20, 19, 9, 10, 11, 2.1, 1, 2.2, 3]
+
+    eta_dict = {}
+
+    for r in row:
+        eta_dict[r.stop_id] = r
+        eta_no_bus += r.eta_bus
+
+    ind = route.index(stop)
+
+    st = route[ind]
+
+    eta_start_end = 0
+
+    while st != end_stop:
+        ind += 1
+
+        if(ind == len(route)):
+            ind = 0
+
+        st = route[ind]
+
+        eta_start_end += eta_dict[float(st)].eta_bus
+
+    return jsonify({"eta": eta_no_bus}, {"eta_s_e": eta_start_end}), 201
 
 @bus.route('/get_bus', methods=['GET'])
 def get_latest_gps_data():
