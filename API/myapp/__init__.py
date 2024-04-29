9778cbf4eeb69a643d522ea1fb9afad6ab80e86f
Michael Terrefortes <michael.terrefortes@upr.edu>
2024-04-29 08:52:25 -0400

    api database update

--- API/myapp/__init__.py
+++ API/myapp/__init__.py
@@ -19,5 +19,8 @@ def create_app():
     #db.app = application
     db.init_app(app)
 
+    with app.app_context():
+        db.create_all()
+
     app.register_blueprint(bus)
 
--- API/myapp/database.py
+++ API/myapp/database.py
@@ -9,3 +9,10 @@ class Bus(db.Model):
     longitude = db.Column(db.Float)
     acceleration = db.Column(db.Float)
     time = db.Column(db.Text)
+
+class ETA(db.Model):
+    stop_id = db.Column(db.Integer)
+    time_slot = db.Column(db.Integer)
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+    eta = db.Column(db.Float)
